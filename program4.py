9dea69313e5fa45e19c9375f825e0eafdaa7850e
shravani0622 <shravanigole2206@gmail.com>
2025-08-07 21:17:03 +0530

    Delete program4.py

--- program4.py
+++
@@ -1,9 +0,0 @@
-#program for fibonacii
-def fibonacii(n):
-    A=[0,1]
-    for i in range(2,n):
-        A.append(A[i-1]+A[i-2])
-    print("Display the fibonacii series",A)
-    return A
-n=int(input("Enter the size of array: "))
-fibonacii(n)
